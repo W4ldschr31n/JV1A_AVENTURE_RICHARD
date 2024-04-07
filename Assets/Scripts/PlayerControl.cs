d3829d3480681a89f5475ac85d80bfb722057142
W4ldschr31n <nicolasrich@hotmail.fr>
2024-04-07 15:26:16 +0200

    [ADD] Player movement blend trees

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,18 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
+    // Internal components
     private Rigidbody2D rgbd;
     private Animator animator;
     private Inventory inventory;
-    public float moveSpeed;
-    [SerializeField]
-    private int maxHealth = 100;
-    public int health;
+    // Events
     public static event Action onPlayerTakeHit;
     public static event Action onPlayerHeal;
     public static event Action onPlayerDead;
+    // Movement
+    public float moveSpeed;
+    private Vector3 direction;
+    private float lastX, lastY;
+    // Health
+    [SerializeField]
+    private int maxHealth = 100;
+    public int health;
+    // State
     public bool canBeDamaged = true;
     public bool canAttack = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +37,7 @@ public class PlayerControl : MonoBehaviour
 
     private void Update()
     {
+        // Attack
         if (canAttack)
         { 
             if (Input.GetButtonDown("Fire1"))
@@ -41,22 +50,46 @@ public class PlayerControl : MonoBehaviour
                 ThrowObole();
             }
         }
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(x, y).normalized;
-        Vector3 movement = direction * moveSpeed * Time.fixedDeltaTime;
-        rgbd.MovePosition(transform.position + movement);
-
+        // Reset
         if (Input.GetKeyDown(KeyCode.R))
         {
             transform.position = Vector2.zero;
         }
+
+        // Movement - the actual moving happens in FixedUpdate() for consistency reasons
+        float dirX = Input.GetAxisRaw("Horizontal");
+        float dirY = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(dirX, dirY).normalized;
+
+        // Animation
+        UpdateAnimation();
+
+    }
+
+    private void UpdateAnimation()
+    {
+        if (direction.magnitude == 0f)
+        { // Not moving
+            animator.SetFloat("LastDirX", lastX);
+            animator.SetFloat("LastDirY", lastY);
+            animator.SetBool("Movement", false);
+        }
+        else
+        { // Moving
+            lastX = direction.x;
+            lastY = direction.y;
+            animator.SetBool("Movement", true);
+        }
+        // Keep track of the faced direction
+        animator.SetFloat("DirX", direction.x);
+        animator.SetFloat("DirY", direction.y);
+    }
+
+    void FixedUpdate()
+    {
+        // Movement - direction is computed in Update() for responsiveness reasons
+        Vector3 movement = direction * moveSpeed * Time.fixedDeltaTime;
+        rgbd.MovePosition(transform.position + movement);
     }
 
     private void AttackJudgement()
